69f22f79ece2528417482948f9b1d0b96fddba34
OGMOL <olegmolokov11@gmail.com>
2022-07-25 11:59:11 +0300

    "fix exception"

--- src/main/java/ValidationSystem.java
+++ src/main/java/ValidationSystem.java
@@ -1,12 +1,12 @@
 public class ValidationSystem {
 
-    static <T> Validator create (T type){
+    static <T> Validator create (T type) throws ValidationFailedException {
         if (type instanceof Integer) {
             return new IntegerValidator();
         }
         if (type instanceof String) {
             return new StringValidator();
-        } else throw new RuntimeException("incorrect type");
+        } else throw new ValidationFailedException("неправильный тип");
     }
 
     public static <T> void validate(T type) throws ValidationFailedException {
